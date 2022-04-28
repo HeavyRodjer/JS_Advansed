f07c7e3b289ee30476f032a9a78930e8cf45aa1d
Heavy <terpeluk03@gmail.com>
2022-04-28 18:20:27 +0300

    add routers

---
+++ Home1/router_example/.history/router-ex/src/components/Edit/index_20220427200023.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>EDIT</div>
+</template>
+
+<script>
+export default {
+  name: "Edit",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ Home1/router_example/.history/router-ex/src/router/index_20220427200714.js
@@ -0,0 +1,21 @@
+// 1. Define route components.
+// These can be imported from other files
+import Home from "@/components/Home";
+import Edit from "@/components/Edit";
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: "/", component: Home },
+  { path: "/edit", component: Edit },
+];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes, // short for `routes: routes`
+});
